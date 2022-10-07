06ae5f603868850f4d1bd76764286bb9999c5a6e
shikarev <shikarev@voltep.ru>
2022-10-07 18:30:41 +0300

    navbar & theme fix

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import cls from './Navbar.module.scss'
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
 
 interface NavbarProps {
   className?: string
@@ -11,12 +10,11 @@ interface NavbarProps {
 const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to='/' className={cls.mainLink}>
           Главная
         </AppLink>
-        <AppLink theme={AppLinkTheme.PRIMARY} to='about'>
+        <AppLink theme={AppLinkTheme.SECONDARY} to='about'>
           О сайте
         </AppLink>
       </div>
